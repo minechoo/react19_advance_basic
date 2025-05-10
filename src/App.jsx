9ea0f6085c714d7743662c32d0ce97e822444049
tubi55 <tubi55@nate.com>
2025-05-10 13:33:13 +0900

    feat: state를 통한 폼의 값이 실시간으로 갱신되는 제어 컴포넌트

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
+
+// useState훅을 통한 state기반으로 화면렌더링이 자동으로 일어나는 제어컴포넌트 생성
 function App() {
+  console.log("re-render");
+  const [Text, setText] = useState("");
+
+  //change 이벤트 발생시 실행할 함수
+  //입력하고 있는 요소의 value값을 실시간으로 Text상태값에 담아주는 함수
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  //Text상태의 값을 강제로 비우는 함수
+  const handleReset = () => {
+    setText("");
+  };
+
   return (
     <>
       <h1>제어 컴포넌트 (Controlled Component)</h1>
+
+      <input type="text" value={Text} onChange={handleChange} />
+      <button onClick={handleReset}>초기화</button>
+      <p>입력값: {Text}</p>
     </>
   );
 }
