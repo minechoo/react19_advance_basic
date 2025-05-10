43521bd1b246ce2f1cdb3099972d6938838d5eff
tubi55 <tubi55@nate.com>
2025-05-10 16:05:44 +0900

    feat: dispatch를 통한 전역 컨텍스트 정보 변경요청

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -6,5 +6,12 @@ export default function Counter() {
   const { GlobalState, dispatch } = useContext(GlobalContext);
   console.log(GlobalState);
 
-  return <div>Counter</div>;
+  return (
+    <div>
+      <h1>{GlobalState.count}</h1>
+      {/* dispatch함수로 type이 있는 액션객체를 전달하면 리듀서 함수가 해당 액션 객체의 타입을 확인해서 전역데이터 변경처리 */}
+      <button onClick={() => dispatch({ type: "DECREMENT" })}>-</button>
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>+</button>
+    </div>
+  );
 }
